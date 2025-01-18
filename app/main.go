5a45dfe9137d6755910c2455352d3b10bbec8d62
Artem Privalov <privalov.dev@gmail.com>
2025-01-18 16:07:16 +0100

    initial structure

--- app/main.go
+++ app/main.go
@@ -6,6 +6,25 @@ import (
 	"github.com/weaviate/weaviate-go-client/v4/weaviate"
 )
 
+type Reference struct {
+	Title     string   `json:"title"`
+	Abstract  string   `json:"abstract"`
+	Authors   []string `json:"authors"`
+	Embedding []int    `json:"embedding"`
+}
+
+type Paper struct {
+	PaperID    int64       `json:"paperId"`
+	Embeddings []int       `json:"embeddings"`
+	Title      string      `json:"title"`
+	Abstract   string      `json:"abstract"`
+	References []Reference `json:"references"`
+}
+
+type DataWrapper struct {
+	Data []Paper `json:"data"`
+}
+
 func GetSchema() {
 	cfg := weaviate.Config{
 		Host:   "localhost:8080",
