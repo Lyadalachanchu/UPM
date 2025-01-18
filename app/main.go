e86d89e0cab77415620babdc6833c666b24b13bb
Artem Privalov <privalov.dev@gmail.com>
2025-01-18 16:04:17 +0100

    manual setup

--- app/main.go
+++ app/main.go
@@ -1,10 +1,28 @@
 package main
 
-type Options struct {
-	Host string
-	Port int
+import (
+	"context"
+	"fmt"
+	"github.com/weaviate/weaviate-go-client/v4/weaviate"
+)
+
+func GetSchema() {
+	cfg := weaviate.Config{
+		Host:   "localhost:8080",
+		Scheme: "http",
+	}
+	client, err := weaviate.NewClient(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	schema, err := client.Schema().Getter().Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v", schema)
 }
 
 func main() {
-
+	GetSchema()
 }
